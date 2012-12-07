37599bc3ff8704236905a478d7ea729b041ab03b
candycode <ugovaretto@gmail.com>
2012-12-07 14:11:06 +0100

    Created branch

--- test/basic-data.py
+++
@@ -1,8 +0,0 @@
-import qpy
-import qpy_test
-obj = qpy_test.QpyTestObject()
-
-print(obj.copyString('a string'))
-print(obj.copyInt(123))
-print(round(obj.copyFloat(1.23),2))
-print(round(obj.copyDouble(12.3),2))
